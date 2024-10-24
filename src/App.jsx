c6586754a221eca9b9d628b778ce820d5b36abce
rimiruma <rimiruma12@gmail.com>
2024-10-24 16:12:47 +0600

    added profile styles done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css'
+import Blogs from './Components/Header/Blogs/Blogs'
+import Header from './Components/Header/Header'
+
 
 function App() {
 
   return (
     <>
-      <h1 className='text-6xl bg-red-300'>Knowledge Cafe</h1>
+    <Header></Header>
+    <Blogs></Blogs>
     </>
   )
 }
